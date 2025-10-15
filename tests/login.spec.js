b1615a2c050e5407f6b9d3dbac5162542074e295
SanderTorg <santorgersen@gmail.com>
2025-10-16 00:56:06 +0200

    GitHub dont see .env file trying to import dotenv

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,4 +1,7 @@
 import { test, expect } from "@playwright/test";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 test("User can log in successfully", async ({ page }) => {
   await page.goto("/login/");
