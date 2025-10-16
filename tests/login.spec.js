6d057abb5b7f3250303bdb6bf9764c04f9310345
SanderTorg <santorgersen@gmail.com>
2025-10-16 02:21:10 +0200

    Fixing login network is to slow to redirect to index

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,35 +1,48 @@
 import { test, expect } from "@playwright/test";
-import dotenv from "dotenv";
 
-dotenv.config();
+const validEmail = process.env.USER_EMAIL;
+const validPassword = process.env.USER_PASSWORD;
 
-test("User can log in successfully", async ({ page }) => {
-  await page.goto("/login/");
-  await expect(page).toHaveURL("/login/");
+test.describe("Login testing with a valid user and not a valid user", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/login/");
+  });
+  test("User can log in successfully", async ({ page }) => {
+    await expect(page).toHaveURL("/login/");
+    expect(validEmail).toBeTruthy();
+    expect(validPassword).toBeTruthy();
 
-  await page.fill('form#loginForm input[name="email"]', process.env.USER_EMAIL);
-  await page.fill(
-    'form#loginForm input[name="password"]',
-    process.env.USER_PASSWORD
-  );
-  await page.click('form#loginForm button[type="submit"]');
+    await page.fill('form#loginForm input[name="email"]', validEmail);
+    await page.fill('form#loginForm input[name="password"]', validPassword);
 
-  await expect(page).toHaveURL("/");
-});
+    await Promise.all([
+      page.waitForResponse(
+        (resp) => resp.url().includes("/login/") && resp.status() === 200
+      ),
+      page.click('button[type="submit"]'),
+    ]);
+    const homePageHeader = page.locator('h1:has-text("Welcome to this site"),');
+    await expect(homePageHeader).toBeVisible({ timeout: 10000 });
 
-test("user cannot log in with invalid credentials", async ({ page }) => {
-  await page.goto("login/");
-  await expect(page).toHaveURL("/login/");
-  await page.fill(
-    'form#loginForm input[name="email"]',
-    "invalid_email@example.com"
-  );
-  await page.fill('form#loginForm input[name="password"]', "invalid_password");
+    await expect(page).not.toContain("/login/");
+  });
 
-  await page.click('form#loginForm button[type="submit"]');
-  await expect(page).toHaveURL("/login/");
+  test("user cannot log in with invalid credentials", async ({ page }) => {
+    await expect(page).toHaveURL("/login/");
+    await page.fill(
+      'form#loginForm input[name="email"]',
+      "invalid_email@example.com"
+    );
+    await page.fill(
+      'form#loginForm input[name="password"]',
+      "invalid_password"
+    );
 
-  const alert = page.locator('#message-container [role="alert"]');
-  await expect(alert).toBeVisible();
-  await expect(alert).not.toHaveText("");
+    await page.click('form#loginForm button[type="submit"]');
+    await expect(page).toHaveURL("/login/");
+
+    const alert = page.locator('#message-container [role="alert"]');
+    await expect(alert).toBeVisible();
+    await expect(alert).not.toHaveText("");
+  });
 });
